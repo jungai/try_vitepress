2a21f3a9741965c0dcba6e40ff958b75034867c6
jungai <juworaphol125@gmail.com>
2020-05-03 14:56:24 +0700

    try basic vitepress

---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <Content/>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const msg =  ref('this is custom layout');
+
+    return { msg }
+  }
+}
+</script>
